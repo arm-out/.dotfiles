bf80cd24c28632f6f8de48872d4bcf946abe9f2b
arm-out <arminsuraj@gmail.com>
2025-03-20 02:07:25 +1100

    flake init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "My personal setup";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {self, nixpkgs, ... }: {
+
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./system/configuration.nix ];
+      };
+    };
+
+  };
+}
