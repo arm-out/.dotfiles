811c0d5d631b56134b15d144c9a66a15cc5b71c6
arm-out <arminsuraj@gmail.com>
2025-03-20 16:01:49 +1100

    temporarily disabled mpri on waybar

--- programs/desktop/default.nix
+++ programs/desktop/default.nix
@@ -2,7 +2,14 @@
 
 {
   home.file = {
+    # Hyprland Config
     ".config/hypr/hyprland.conf".source = ./hyprland.conf;
+
+    # Waybar Config
+    ".config/waybar/config.jsonc".source = ./config.jsonc;
+    ".config/waybar/colors.css".source = ./colors.css;
+    ".config/waybar/style.css".source = ./style.css;
+    ".config/waybar/wittr.sh".source = ./wittr.sh;
   };
 
   home.packages = with pkgs; [
