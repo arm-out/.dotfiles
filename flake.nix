366cdc5fa38e48a92cc7ca12f6488f34bf420d3d
arm-out <arminsuraj@gmail.com>
2025-03-28 11:49:02 +1100

    broken nixCats

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixCats.url = "github:BirdeeHub/nixCats-nvim";
   };
 
   outputs = {self, nixpkgs, home-manager, ... }:
--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./programs
+    ./nvim
   ];
 
   home.username = userSettings.username;
