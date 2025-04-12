853867a8e83f154afb5448e98bf58ec9f0a67ab2
arm-out <arminsuraj@gmail.com>
2025-04-12 14:25:39 +1000

    remove fish init message

--- programs/shell/default.nix
+++ programs/shell/default.nix
@@ -5,13 +5,16 @@
     ghostty
   ];
   
-  programs.fish.enable = true;
-  programs.fish.shellAliases = {
-    r = "sudo nixos-rebuild switch --flake ~/.dotfiles";
-    gc = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
-    opt = "nix-store --optimise";
-    dot = "cd ~/.dotfiles";
-    ".." = "cd ..";
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      r = "sudo nixos-rebuild switch --flake ~/.dotfiles";
+      gc = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
+      opt = "nix-store --optimise";
+      dot = "cd ~/.dotfiles";
+      ".." = "cd ..";
+    };
+    interactiveShellInit = "set fish_greeting";
   };
 }
 
