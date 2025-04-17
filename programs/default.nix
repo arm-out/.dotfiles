07a5949289ede2ef4b34ebc1fda24c7e8e7b722d
arm-out <arminsuraj@gmail.com>
2025-04-17 15:10:56 +1000

    spotify

--- programs/default.nix
+++ programs/default.nix
@@ -6,11 +6,23 @@
     ./shell
     #./nvim
     ./services.nix
+    inputs.spicetify-nix.homeManagerModules.default
   ];
 
   home.packages = with pkgs; [
     inputs.zen-browser.packages."${system}".default
     discord
   ];
+
+  programs.spicetify = 
+  let
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+  in
+  {
+    enable = true;
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+    ];
+  };
 
 }
